b2040d0d39e1c5ac58ef107b1726a832b22d83e2
GJRKZB <Robinkonijnendijk@gmail.com>
2024-05-21 22:18:38 +0200

    handle cookie session methods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { getSession } from "./lib/session";
 
 export default async function page() {
+  const session = await getSession();
+
   return (
     <div>
       <NextUIProvider>
-        <div>Main</div>
+        <div>
+          <button>Logout</button>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+        </div>
       </NextUIProvider>
     </div>
   );
