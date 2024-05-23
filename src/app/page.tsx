4e58b89cd874af9700f53cbaaa4619152fa9f2b4
GJRKZB <Robinkonijnendijk@gmail.com>
2024-05-23 18:04:55 +0200

    add NextUIProvider to layout and update page component for authentication functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
-import { NextUIProvider } from "@nextui-org/react";
-import { getSession } from "./lib/session";
+import { getSession, deleteSession } from "./lib/session";
 
 export default async function page() {
   const session = await getSession();
 
   return (
     <div>
-      <NextUIProvider>
-        <div>
-          <button>Logout</button>
-          <pre>{JSON.stringify(session, null, 2)}</pre>
-        </div>
-      </NextUIProvider>
+      <div>
+        <h1>{session ? `Welcome, ${session.username}` : `Not logged in`}</h1>
+        <br />
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+        <br />
+        <form
+          action={async () => {
+            "use server";
+            await deleteSession();
+          }}
+        >
+          <br />
+          <button type="submit">Logout</button>
+        </form>
+      </div>
     </div>
   );
 }
