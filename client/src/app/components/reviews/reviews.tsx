637334986aaa4e2190a160e4baaf365a3aa53343
GJRKZB <Robinkonijnendijk@gmail.com>
2024-06-05 21:10:11 +0200

    add authentication logic and update client for reviews component

--- client/src/app/components/reviews/reviews.tsx
+++ client/src/app/components/reviews/reviews.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { Button, Slider } from "@nextui-org/react";
 import axios from "axios";
+import { useAuth } from "@/app/hooks/useAuth";
 
 interface ReviewSliderProps {
   title: string;
@@ -10,6 +11,7 @@ interface ReviewSliderProps {
 }
 
 const Reviews: React.FC<ReviewSliderProps> = ({ title, reviews }) => {
+  const { isAuthenticated, user, loading } = useAuth();
   const [message, setMessage] = useState<string>("");
   const [fetchedData, setFetchedData] = useState<string>("");
   const [values, setValues] = useState<number[]>(() => {
@@ -90,42 +92,48 @@ const Reviews: React.FC<ReviewSliderProps> = ({ title, reviews }) => {
 
   return (
     <div className="w-full flex flex-col items-center gap-10">
-      <div className="w-full flex justify-center flex-col gap-2 items-center">
-        <p className="font-bold text-base text-black">Your rating:</p>
-        <div className="bg-black rounded-full w-20 h-20 items-center flex justify-center">
-          <p className="font-bold text-base text-white">{average}</p>
-        </div>
-      </div>
-      <div className="w-full flex justify-center flex-col text-mono items-center gap-2">
-        {reviews.map((review, index) => (
-          <div
-            key={review.id}
-            className="w-full flex flex-col items-center gap-2"
-          >
-            <Slider
-              size="lg"
-              step={0.5}
-              color="foreground"
-              label={review.label}
-              showSteps={true}
-              maxValue={5}
-              minValue={0}
-              value={values[index] || 0.0}
-              className="max-w-md"
-              onChange={(value) => handleChange(index)(value as number)}
-            />
+      {isAuthenticated ? (
+        <div className="w-full flex justify-center flex-col text-mono items-center gap-2">
+          <div className="w-full flex justify-center flex-col gap-2 items-center">
+            <p className="font-bold text-base text-black">Your rating:</p>
+            <div className="bg-black rounded-full w-20 h-20 items-center flex justify-center">
+              <p className="font-bold text-base text-white">{average}</p>
+            </div>
           </div>
-        ))}
-      </div>
-      <p className="text-base text-black">{fetchedData}</p>
-      <p className="text-base text-black">{message}</p>
-      <Button
-        className="text-normal text-white bg-black p-8"
-        radius="full"
-        onClick={handleSubmit}
-      >
-        Submit
-      </Button>
+          {reviews.map((review, index) => (
+            <div
+              key={review.id}
+              className="w-full flex flex-col items-center gap-2"
+            >
+              <Slider
+                size="lg"
+                step={0.5}
+                color="foreground"
+                label={review.label}
+                showSteps={true}
+                maxValue={5}
+                minValue={0}
+                value={values[index] || 0.0}
+                className="max-w-md"
+                onChange={(value) => handleChange(index)(value as number)}
+              />
+            </div>
+          ))}
+          <p className="text-base text-black">{fetchedData}</p>
+          <p className="text-base text-black">{message}</p>
+          <Button
+            className="text-normal text-white bg-black p-8"
+            radius="full"
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
+        </div>
+      ) : (
+        <div>
+          <p>You need to be logged in to submit a review.</p>
+        </div>
+      )}
     </div>
   );
 };
