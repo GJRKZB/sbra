a1333f66dcbf8f412347228fa1aef2946c339bbf
GJRKZB <Robinkonijnendijk@gmail.com>
2024-06-10 09:56:42 +0200

    fetch and display average on restaurant list

--- client/src/app/components/cards/restaurant/restaurantCard.tsx
+++ client/src/app/components/cards/restaurant/restaurantCard.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import React, { useState, useEffect } from "react";
 import { Card, CardFooter, Image } from "@nextui-org/react";
 import ViewRestaurantBtn from "../../buttons/restaurant/viewRestaurantBtn";
+import axios from "axios";
 
 interface RestaurantCardProps {
   title: string;
   description: string;
   image?: string;
-  review?: number;
   slug: string;
 }
 
@@ -15,9 +16,29 @@ const RestaurantCard: React.FC<RestaurantCardProps> = ({
   title,
   description,
   image,
-  review,
   slug,
 }) => {
+  const [average, setAverage] = useState<number | null>(0);
+
+  useEffect(() => {
+    const fetchAverageReview = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/api/average-reviews`
+        );
+        const avgReview = response.data.averageReviews.find(
+          (avg: any) => avg.title === title
+        );
+        if (avgReview) {
+          setAverage(avgReview.averageReview);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchAverageReview();
+  }, [title]);
+
   return (
     <div className="h-96">
       <Card className="h-full font-mono text-start" isFooterBlurred>
@@ -31,7 +52,9 @@ const RestaurantCard: React.FC<RestaurantCardProps> = ({
           <div>
             <h1 className="text-xl font-bold">{title}</h1>
             <p className="text-base font-normal">{description}</p>
-            <p className="text-sm">{review}</p>
+            <p className="text-sm">
+              {average !== null ? average.toFixed(1) : 0}
+            </p>
           </div>
           <ViewRestaurantBtn slug={slug} />
         </CardFooter>
