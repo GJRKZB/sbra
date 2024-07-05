e28b026c4e4980ec94232f73e35ba1e8e5a67c73
GJRKZB <Robinkonijnendijk@gmail.com>
2024-07-05 16:46:35 +0200

    fetch sorted restaurant list

--- client/src/app/components/cards/restaurant/restaurantList.tsx
+++ client/src/app/components/cards/restaurant/restaurantList.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { fetchRestaurants } from "@/app/utils/api";
 import RestaurantCard from "./restaurantCard";
 
@@ -10,26 +13,46 @@ interface IRestaurant {
   totalAverage: number;
 }
 
-async function getRestaurants() {
-  const res = await fetchRestaurants();
-  return res.restaurants;
-}
+export default function Restaurants() {
+  const [restaurants, setRestaurants] = useState<IRestaurant[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-export default async function Restaurants() {
-  const restaurants = await getRestaurants();
+  async function fetchAndSetRestaurants() {
+    setIsLoading(true);
+    try {
+      const fetchedData = await fetchRestaurants();
+      if (fetchedData && Array.isArray(fetchedData.restaurants)) {
+        setRestaurants(fetchedData.restaurants);
+      } else {
+        console.error("Unexpected data structure:", fetchedData);
+        setError("Unexpected data structure from fetch.");
+      }
+    } catch (error) {
+      console.error("Error fetching restaurants:", error);
+      setError("Error fetching restaurants.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchAndSetRestaurants();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading restaurants...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className="flex flex-col gap-10">
-      {restaurants ? (
-        restaurants.map((restaurant: IRestaurant) => (
-          <RestaurantCard
-            key={restaurant._id}
-            restaurantTitle={restaurant.restaurantTitle}
-            description={restaurant.description}
-            image={restaurant.image}
-            slug={restaurant.slug}
-            totalAverage={restaurant.totalAverage}
-          />
+      {restaurants.length > 0 ? (
+        restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant._id} {...restaurant} />
         ))
       ) : (
         <div>No restaurants found....</div>
