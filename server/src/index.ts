8fe6dcbfd8910ec6c8dd15d8f692043caf6cdd52
GJRKZB <Robinkonijnendijk@gmail.com>
2024-07-06 18:13:11 +0200

    update CORS configuration to use environment-specific origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,15 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors({ origin: "*" }));
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "production"
+      ? process.env.PROD_ORIGIN
+      : process.env.DEV_ORIGIN,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
